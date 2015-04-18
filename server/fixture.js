9ed13ce9f93ca0bb7d4a23e3b7b7c7686560987c
Kelvin Lo <kel@Kelvins-MacBook-Air.local>
2015-04-18 19:17:48 +0800

    Change fixtures

--- server/fixture.js
+++ server/fixture.js
@@ -1,23 +1,29 @@
 ClassesList.insert({
   date: 1430478000,
   time: "11:00 PM",
+  length: "60 min",
   gym: "Pure",
   name: "Epic Kick Boxing",
+  instructor: "Yoshi",
   location: "Central"
 });
 
 ClassesList.insert({
   date: 1430481600,
   time: "12:00 PM",
+  length: "60 min",
   gym: "Pure",
   name: "Yoga",
+  instructor: "Dom",
   location: "Central"
 });
 
 ClassesList.insert({
   date: 1430485200,
   time: "13:00 PM",
+  length: "60 min",
   gym: "Pure",
   name: "Cycling",
+  instructor: "Kelvin",
   location: "Central"
 });
