9a7a2595c49e928cb0985ef9e614344c0dfb64f2
Kelvin Lo <kel@Kelvins-MacBook-Air.local>
2015-04-19 11:44:01 +0800

    Styling changes

--- server/fixture.js
+++ server/fixture.js
@@ -1,77 +1,129 @@
 ClassesList.insert({
-  time: "10:30 AM",
+  time: "5:30 PM",
   duration: "60 Min",
   length: "30 Min",
   distance: "321m",
-  gym: "Pure",
+  gym: "BOUTIQUE YOGA STUDIO",
   name: "Yoga",
-  instructor: "Yoshi",
-  region: "Central",
-  address: "16/F The Centrium, 60 Wyndham Street, Central",
+  instructor: "Baznini Kundalini",
+  region: "Quarry Bay",
+  address: "16/F Hoi Tai Street, Quarry Bay",
   image: "/images/jane.jpg"
 });
 
 ClassesList.insert({
-  time: "11:00 AM",
+  time: "5:30 PM",
   duration: "75 Min",
   length: "60 Min",
   distance: "234m",
-  gym: "ATMA Studio",
+  gym: "MARTIAL ARTS STUDIO",
   name: "Epic Kick Boxing",
-  instructor: "Dom",
-  region: "Central",
+  instructor: "Paksam Nukrap",
+  region: "Quarry Bay",
   address: "49-51 Jervois Street, Central",
   image: "/images/kickboxing.jpg"
 });
 
 ClassesList.insert({
-  time: "12:00 PM",
+  time: "6:00 PM",
   duration: "60 Min",
   length: "2 Hours",
   distance: "321m",
   gym: "Pure",
   name: "Yoga",
   instructor: "Yoshi",
-  region: "Central",
+  region: "Quarry Bay",
   address: "16/F The Centrium, 60 Wyndham Street, Central",
   image: "/images/jane.jpg"
 });
 
 ClassesList.insert({
-  time: "13:00 PM",
+  time: "6:00 PM",
   duration: "30 Min",
   length: "3 Hours",
   distance: "124m",
   gym: "Pure",
   name: "Cycling",
   instructor: "Kelvin",
-  region: "Central",
+  region: "Quarry Bay",
   address: "16/F The Centrium, 60 Wyndham Street, Central",
   image: "/images/spin.jpg"
 });
 
 ClassesList.insert({
-  time: "13:00 PM",
+  time: "7:00 PM",
   duration: "45 Min",
   length: "3 Hours",
   distance: "264m",
   gym: "California",
   name: "Cycling",
   instructor: "Cassie",
-  region: "Central",
+  region: "Quarry Bay",
   address: "139 Queens Road, Central",
   image: "/images/spin.jpg"
 });
 
 ClassesList.insert({
-  time: "16:00 PM",
+  time: "7:00 PM",
   duration: "60 Min",
   length: "7 Hours",
   distance: "234m",
   gym: "ATMA Studio",
   name: "Epic Kick Boxing",
   instructor: "Dom",
-  region: "Central",
+  region: "Quarry Bay",
+  address: "49-51 Jervois Street, Central",
+  image: "/images/kickboxing.jpg"
+});
+
+ClassesList.insert({
+  time: "7:00 PM",
+  duration: "45 Min",
+  length: "3 Hours",
+  distance: "264m",
+  gym: "California",
+  name: "Cycling",
+  instructor: "Cassie",
+  region: "Quarry Bay",
+  address: "139 Queens Road, Central",
+  image: "/images/spin.jpg"
+});
+
+ClassesList.insert({
+  time: "7:00 PM",
+  duration: "60 Min",
+  length: "7 Hours",
+  distance: "234m",
+  gym: "ATMA Studio",
+  name: "Epic Kick Boxing",
+  instructor: "Dom",
+  region: "Quarry Bay",
+  address: "49-51 Jervois Street, Central",
+  image: "/images/kickboxing.jpg"
+});
+
+ClassesList.insert({
+  time: "7:00 PM",
+  duration: "45 Min",
+  length: "3 Hours",
+  distance: "264m",
+  gym: "California",
+  name: "Cycling",
+  instructor: "Cassie",
+  region: "Quarry Bay",
+  address: "139 Queens Road, Central",
+  image: "/images/spin.jpg"
+});
+
+ClassesList.insert({
+  time: "7:00 PM",
+  duration: "60 Min",
+  length: "7 Hours",
+  distance: "234m",
+  gym: "ATMA Studio",
+  name: "Epic Kick Boxing",
+  instructor: "Dom",
+  region: "Quarry Bay",
   address: "49-51 Jervois Street, Central",
   image: "/images/kickboxing.jpg"
 });
